eb64b42dd7f5cc3adf22857285017ed566a5ac2c
Tulasi Marupudi <marupudit@gmail.com>
2022-03-21 11:48:52 +0000

    Add project files.

---
+++ WPFDemo/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(pass.Password !="" || user.Text != "")
+            {
+                if (pass.Password == "swathi" && user.Text == "swathi")
+                {
+                    MessageBox.Show("Welcome Swathi", "Authenication Success");
+                }
+                else MessageBox.Show("Please enter valid credentails", "Invalid credentails");
+            }
+            else MessageBox.Show("Please enter valid credentails", "Invalid credentails");
+        }
+    }
+}
